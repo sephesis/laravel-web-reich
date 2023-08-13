00dc67c45aa772da96dbfa02c2f5a3fe8fcaf248
a.kurenkova <sephesis@yandex.ru>
2023-08-13 12:46:46 +0300

    faq template

---
+++ resources/views/faq/create.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.admin')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Добавить вопрос/ответ</h1>
+            </div><!-- /.col -->
+          <!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+  
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          @if ($errors->any())
+          <div class="alert alert-danger">
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+          </div>
+      @endif
+      @if (\Session::has('success'))
+      <div class="alert alert-success" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+        <h4><i class="icon fa fa-check"></i>{{ \Session::get('success') }}</h4>
+      </div>
+      @endif
+          <!-- Small boxes (Stat box) -->
+          <div class="row">
+             <div class="col-md-6">
+              <form action="{{ route('faq.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                  <input type="text" name="title" placeholder="Текст вопроса" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" name="sort" placeholder="Сортировка" value="500" class="form-control">
+                  </div>
+
+                <div class="form-group">
+                    <textarea type="text" rows="6" name="description" placeholder="Ответ" class="form-control"></textarea>
+                  </div>
+            
+                <div class="form-group">
+                  <input type="submit" value="Сохранить" class="btn btn-primary">
+                </div>
+              </form>
+             </div>
+          </div>
+          <!-- /.row -->
+       
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+@endsection
